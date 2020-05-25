1d3afb7dfaf5213bb78200179bdc4a57fc34398b
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-05-24 21:10:37 -0600

    feat:homepage, about page and ngo dashboard

---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '100%';
+    document.getElementById('side-menu').style.height = '100%';
+}
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+}
